1e3dc4053ecfc45fb66bf8a8d7d5e25b085362cf
AmiHosaka <ami39raba@outlook.jp>
2022-02-14 14:17:24 +0900

    投稿一覧を表示する機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
     @post = Post.new(title: params[:title], content: params[:content], category: params[:category], user_id: params[:user_id])
     @post.save
   end
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post 'posts/create' => 'posts#create'
+  get 'posts/index' => 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
