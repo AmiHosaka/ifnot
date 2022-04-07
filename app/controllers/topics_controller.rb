cf13ea3eaaebfec8cdbccff74d77603569a45716
AmiHosaka <ami39raba@outlook.jp>
2022-04-07 16:16:40 +0900

    topic_controllerの#create,#index追加,index viewファイル追加

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,9 @@
 class TopicsController < ApplicationController
+  def create
+    @topic = Topic.new(topic_name: params[:topic_name], category_id: params[:category_id], topic_owner: params[:topic_owner])
+    @topic.save
+  end
+  def index
+    @topics = Topic.all.order(created_at: :desc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'posts/index' => 'posts#index'
   get 'posts/:id' => 'posts#show'
   post 'posts/:id/destroy' => 'posts#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post 'topics/create' => 'topics#create'
+  get 'topics/index' => 'topics#index'
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
