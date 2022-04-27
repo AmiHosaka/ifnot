02f2da586afb866e4c0e8d8b7d63a8148d0c0cb6
AmiHosaka <ami39raba@outlook.jp>
2022-04-28 08:53:30 +0900

    categoryの一覧表示と分類毎の議題一覧表示

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,12 @@
 class CategoriesController < ApplicationController
+
+    def index
+        @categories = Category.all.order(created_at: :asc)
+    end
+
+    def show
+        @category = Category.find_by(id: params[:id])
+        @topics = Topic.where(category_id: params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  post 'posts/create' => 'posts#create'
-  get 'posts/index' => 'posts#index'
-  get 'posts/:id' => 'posts#show'
-  post 'posts/:id/destroy' => 'posts#destroy'
 
-  post 'topics/create' => 'topics#create'
-  get 'topics/index' => 'topics#index'
-  get 'topics/:id' => 'topics#show'
+  get 'categories/index' => 'categories#index' #分類を一覧表示する
+  get 'categories/:id' => 'categories#show' #選択した分類に属する議題を一覧表示する
+
+  post 'topics/create' => 'topics#create' #新しく議題を作成する
+  get 'topics/index' => 'topics#index' #議題を一覧表示する
+  get 'topics/:id' => 'topics#show' #選択した議題に属する投稿を一覧表示する
+
+  post 'posts/create' => 'posts#create' #新しく投稿を作成する
+  get 'posts/index' => 'posts#index' #投稿を一覧表示する
+  get 'posts/:id' => 'posts#show' #投稿詳細を表示する
+  post 'posts/:id/destroy' => 'posts#destroy' #投稿を削除する
+
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
