8076146566c5f423f58acb420a5d19094d890f81
AmiHosaka <ami39raba@outlook.jp>
2022-06-11 20:22:50 +0900

    add_belongs_toへ修正

--- db/migrate/20220407053008_add_username_to_posts.rb
+++ db/migrate/20220407053008_add_username_to_posts.rb
@@ -1,6 +1,6 @@
 class AddUsernameToPosts < ActiveRecord::Migration[5.2]
   def change
     add_column :posts, :username, :text
-    belongs_to :topic, foreign_key: true
+    add_belongs_to :topic, foreign_key: true
   end
 end
